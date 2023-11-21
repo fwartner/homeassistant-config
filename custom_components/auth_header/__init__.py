c71bf344436b0411a2c548781bde9eadb6e1989e
Florian Wartner <florian@wartner.io>
2023-11-21 09:59:55 +0100

    Automated: HA Config as at 21/11/2023 09:59

---
+++ custom_components/auth_header/__init__.py
@@ -0,0 +1,144 @@
+import logging
+from http import HTTPStatus
+from ipaddress import ip_address
+from typing import Any, OrderedDict, TYPE_CHECKING
+
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from aiohttp.web import Request, Response
+from homeassistant import data_entry_flow
+from homeassistant.components.auth import DOMAIN as AUTH_DOMAIN
+from homeassistant.components.auth import indieauth
+from homeassistant.components.auth.login_flow import LoginFlowIndexView
+from homeassistant.components.http.ban import log_invalid_auth
+from homeassistant.components.http.data_validator import RequestDataValidator
+from homeassistant.core import HomeAssistant
+
+from . import headers
+
+if TYPE_CHECKING:
+    from homeassistant.components.http import FastUrlDispatcher
+    from aiohttp.web_urldispatcher import UrlDispatcher, AbstractResource
+
+DOMAIN = "auth_header"
+_LOGGER = logging.getLogger(__name__)
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Optional(
+                    "username_header", default="X-Forwarded-Preferred-Username"
+                ): cv.string,
+                vol.Optional("debug", default=False): cv.boolean,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+async def async_setup(hass: HomeAssistant, config):
+    """Register custom view which includes request in context"""
+    # Because we start after auth, we have access to store_result
+    store_result = hass.data[AUTH_DOMAIN]
+    router: "FastUrlDispatcher" | "UrlDispatcher" = hass.http.app.router
+    # Remove old LoginFlowIndexView
+    # HASS < 2023.8 just has a list of all routes, which we can directly remove from
+    for route in router._resources:
+        if route.canonical == RequestLoginFlowIndexView.url:
+            _LOGGER.debug("Removed original login_flow route (UrlDispatcher) %s", route)
+            hass.http.app.router._resources.remove(route)
+    # HASS 2023.8+ uses the "FastUrlDispatcher", which also keeps a dict for faster lookups
+    if hasattr(router, "_resource_index"):
+        resource_index: dict[str, list["AbstractResource"]] = router._resource_index
+        routes = resource_index.get(RequestLoginFlowIndexView.url, None)
+        if routes:
+            for route in routes:
+                if route.canonical == RequestLoginFlowIndexView.url:
+                    _LOGGER.debug("Removed original login_flow route (FastUrlDispatcher) %s", route)
+                    routes.remove(route)
+    _LOGGER.debug("Add new login_flow route")
+    hass.http.register_view(
+        RequestLoginFlowIndexView(
+            hass.auth.login_flow, store_result, config[DOMAIN]["debug"]
+        )
+    )
+
+    # Inject Auth-Header provider.
+    providers = OrderedDict()
+    provider = headers.HeaderAuthProvider(
+        hass,
+        hass.auth._store,
+        config[DOMAIN],
+    )
+    providers[(provider.type, provider.id)] = provider
+    providers.update(hass.auth._providers)
+    hass.auth._providers = providers
+    _LOGGER.debug("Injected auth_header provider")
+    return True
+
+
+def get_actual_ip(request: Request) -> str:
+    """Get remote from `request` without considering overrides. This is because
+    when behind a reverse proxy, hass overrides the .remote attributes with the X-Forwarded-For
+    value. We still need to check the actual remote though, to verify its from a valid proxy.
+    """
+    if isinstance(request._transport_peername, (list, tuple)):
+        return request._transport_peername[0]
+    return request._transport_peername
+
+
+class RequestLoginFlowIndexView(LoginFlowIndexView):
+    debug: bool
+
+    def __init__(self, flow_mgr, store_result, debug=False) -> None:
+        super().__init__(flow_mgr, store_result)
+        self.debug = debug
+
+    @RequestDataValidator(
+        vol.Schema(
+            {
+                vol.Required("client_id"): str,
+                vol.Required("handler"): vol.Any(str, list),
+                vol.Required("redirect_uri"): str,
+                vol.Optional("type", default="authorize"): str,
+            }
+        )
+    )
+    @log_invalid_auth
+    async def post(self, request: Request, data: dict[str, Any]) -> Response:
+        """Create a new login flow."""
+        client_id: str = data["client_id"]
+        redirect_uri: str = data["redirect_uri"]
+
+        if not indieauth.verify_client_id(client_id):
+            return self.json_message("Invalid client id", HTTPStatus.BAD_REQUEST)
+
+        handler: tuple[str, ...] | str
+        if isinstance(data["handler"], list):
+            handler = tuple(data["handler"])
+        else:
+            handler = data["handler"]
+
+        try:
+            _LOGGER.debug(request.headers)
+            actual_ip = get_actual_ip(request)
+            _LOGGER.debug("Got actual IP %s", actual_ip)
+            result = await self._flow_mgr.async_init(
+                handler,  # type: ignore[arg-type]
+                context={
+                    "request": request,
+                    "ip_address": ip_address(request.remote),  # type: ignore[arg-type]
+                    "conn_ip_address": ip_address(actual_ip),  # type: ignore[arg-type]
+                    "credential_only": data.get("type") == "link_user",
+                    "redirect_uri": redirect_uri,
+                },
+            )
+        except data_entry_flow.UnknownHandler:
+            return self.json_message("Invalid handler specified", HTTPStatus.NOT_FOUND)
+        except data_entry_flow.UnknownStep:
+            return self.json_message(
+                "Handler does not support init", HTTPStatus.BAD_REQUEST
+            )
+
+        return await self._async_flow_result_to_response(request, client_id, result)
---
+++ custom_components/auth_header/headers.py
@@ -0,0 +1,151 @@
+"""Header Authentication provider.
+
+Allow access to users based on a header set by a reverse-proxy.
+"""
+import logging
+from typing import Any, Dict, List, Optional, cast
+
+from aiohttp.web import Request
+from homeassistant.auth.models import Credentials, User, UserMeta
+from homeassistant.auth.providers import AUTH_PROVIDERS, AuthProvider, LoginFlow
+from homeassistant.auth.providers.trusted_networks import (
+    InvalidAuthError,
+    InvalidUserError,
+    IPAddress,
+)
+from homeassistant.core import callback
+
+CONF_USERNAME_HEADER = "username_header"
+_LOGGER = logging.getLogger(__name__)
+
+
+@AUTH_PROVIDERS.register("header")
+class HeaderAuthProvider(AuthProvider):
+    """Header Authentication Provider.
+
+    Allow access to users based on a header set by a reverse-proxy.
+    """
+
+    DEFAULT_TITLE = "Header Authentication"
+
+    @property
+    def type(self) -> str:
+        return "auth_header"
+
+    @property
+    def support_mfa(self) -> bool:
+        """Header Authentication Provider does not support MFA."""
+        return False
+
+    async def async_login_flow(self, context: Optional[Dict]) -> LoginFlow:
+        """Return a flow to login."""
+        assert context is not None
+        header_name = self.config[CONF_USERNAME_HEADER]
+        request = cast(Request, context.get("request"))
+        if header_name not in request.headers:
+            _LOGGER.info("No header set, returning empty flow")
+            return HeaderLoginFlow(
+                self,
+                None,
+                [],
+                cast(IPAddress, context.get("conn_ip_address")),
+            )
+        remote_user = request.headers[header_name].casefold()
+        # Translate username to id
+        users = await self.store.async_get_users()
+        available_users = [
+            user for user in users if not user.system_generated and user.is_active
+        ]
+        return HeaderLoginFlow(
+            self,
+            remote_user,
+            available_users,
+            cast(IPAddress, context.get("conn_ip_address")),
+        )
+
+    async def async_user_meta_for_credentials(
+        self, credentials: Credentials
+    ) -> UserMeta:
+        """Return extra user metadata for credentials.
+
+        Trusted network auth provider should never create new user.
+        """
+        raise NotImplementedError
+
+    async def async_get_or_create_credentials(
+        self, flow_result: Dict[str, str]
+    ) -> Credentials:
+        """Get credentials based on the flow result."""
+        user_id = flow_result["user"]
+
+        users = await self.store.async_get_users()
+        for user in users:
+            if not user.system_generated and user.is_active and user.id == user_id:
+                for credential in await self.async_credentials():
+                    if credential.data["user_id"] == user_id:
+                        return credential
+                cred = self.async_create_credentials({"user_id": user_id})
+                await self.store.async_link_user(user, cred)
+                return cred
+
+        # We only allow login as exist user
+        raise InvalidUserError
+
+    @callback
+    def async_validate_access(self, ip_addr: IPAddress) -> None:
+        """Make sure the access is from trusted_proxies.
+
+        Raise InvalidAuthError if not.
+        Raise InvalidAuthError if trusted_proxies is not configured.
+        """
+        if not self.hass.http.trusted_proxies:
+            _LOGGER.warning("trusted_proxies is not configured")
+            raise InvalidAuthError("trusted_proxies is not configured")
+
+        if not any(
+            ip_addr in trusted_network
+            for trusted_network in self.hass.http.trusted_proxies
+        ):
+            _LOGGER.warning("Remote IP not in trusted proxies: %s", ip_addr)
+            raise InvalidAuthError("Not in trusted_proxies")
+
+
+class HeaderLoginFlow(LoginFlow):
+    """Handler for the login flow."""
+
+    def __init__(
+        self,
+        auth_provider: HeaderAuthProvider,
+        remote_user: str,
+        available_users: List[User],
+        ip_address: IPAddress,
+    ) -> None:
+        """Initialize the login flow."""
+        super().__init__(auth_provider)
+        self._available_users = available_users
+        self._remote_user = remote_user
+        self._ip_address = ip_address
+
+    async def async_step_init(
+        self, user_input: Optional[Dict[str, str]] = None
+    ) -> Dict[str, Any]:
+        """Handle the step of the form."""
+        try:
+            cast(HeaderAuthProvider, self._auth_provider).async_validate_access(
+                self._ip_address
+            )
+
+        except InvalidAuthError as exc:
+            _LOGGER.debug("invalid auth", exc_info=exc)
+            return self.async_abort(reason="not_allowed")
+
+        for user in self._available_users:
+            for cred in user.credentials:
+                if "username" in cred.data:
+                    if cred.data["username"] == self._remote_user:
+                        return await self.async_finish({"user": user.id})
+            if user.name == self._remote_user:
+                return await self.async_finish({"user": user.id})
+
+        _LOGGER.debug("no matching user found")
+        return self.async_abort(reason="not_allowed")
